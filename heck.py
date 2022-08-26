529074d6d0cf8df5927b448653f32ea858375863
Mrjoker404 <111546547+Mrjoker404@users.noreply.github.com>
2022-08-26 21:50:40 +0700

    Create heck.py

---
+++ heck.py
@@ -0,0 +1,39 @@
+#TKJ Topi Hitam
+
+impor os, sys, waktu, datetime, acak, hashlib, re, threading, json, getpass, urllib, permintaan, mekanisasi
+
+dari multiprocessing.pool impor ThreadPool
+
+dari request.exceptions impor ConnectionError
+
+dari browser impor mekanis
+
+isi ulang (sys)
+
+sys.setdefaultencoding('utf8')
+
+br = mekanisasi.Browser()
+
+br.set_handle_robots(Salah)
+
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Versi/12.16')]
+
+def keluar():
+
+    print '\x1b[1;91m[!] Keluar'
+
+    os.sys.keluar()
+
+def  jalan ( z ):
+kami
+    untuk  e  dalam  z  +  '\n' :
+
+        sys.stdout.write(e)
+
+        sys.stdout.flush()
+
+        waktu.tidur(0.01)
+
+logo   =   'JOKER404 </> ASLI ASLI'
